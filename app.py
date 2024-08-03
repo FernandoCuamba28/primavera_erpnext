2030ecf0a77c241e28f638c068ad4b31a3ba0e21
FernandoCuamba28 <44166972+FernandoCuamba28@users.noreply.github.com>
2024-08-03 09:03:09 +0200

    ok

--- app.py
+++ app.py
@@ -24,6 +24,90 @@ headers = {
 }
 
 
+@app.route('/payments', methods=['GET'])
+def payments():
+    endpoint = "/api/resource/Payment Entry"
+    url = base_url + endpoint
+
+    try:
+        # Primeiro, obtenha a lista de IDs
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+
+        # Extraia os IDs dos pagamentos
+        payment_ids = [entry.get('name') for entry in data.get('data', [])]
+
+        # Obtenha detalhes para cada ID
+        detailed_payments = []
+        for payment_id in payment_ids:
+            detail_url = f"{base_url}/api/resource/Payment Entry/{payment_id}"
+            try:
+                detail_response = requests.get(detail_url, headers=headers)
+                detail_response.raise_for_status()
+                payment_details = detail_response.json()
+
+                # Adiciona o ID aos detalhes do pagamento
+                payment_data = payment_details.get('data', {})
+                payment_data['id'] = payment_id
+
+                # Adiciona os dados de referência da fatura
+                invoice_reference_name = None
+                payment_references = payment_data.get('references', [])
+                for reference in payment_references:
+                    if reference.get('reference_doctype') == 'Sales Invoice':
+                        invoice_reference_name = reference.get('reference_name')
+                        break  # Presumindo que há apenas uma fatura por pagamento
+
+                # Buscar dados do cliente e o campo 'custom_codigo'
+                customer_id = payment_data.get('party')  # Ajuste o nome do campo se necessário
+                if customer_id:
+                    customer_url = f"{base_url}/api/resource/Customer/{customer_id}"
+                    try:
+                        customer_response = requests.get(customer_url, headers=headers)
+                        customer_response.raise_for_status()
+                        customer_data = customer_response.json()
+                        custom_codigo = customer_data.get('data', {}).get('custom_codigo', None)
+                        payment_data['custom_codigo'] = custom_codigo
+                    except requests.exceptions.RequestException as customer_error:
+                        payment_data['custom_codigo_error'] = str(customer_error)
+
+                # Formata o resultado conforme solicitado
+                formatted_payment = {
+                    'id': payment_data.get('id'),
+                    'naming_series': payment_data.get('naming_series'),
+                    'payment_type': payment_data.get('payment_type'),
+                    'posting_date': payment_data.get('posting_date'),
+                    'company': payment_data.get('company'),
+                    'paid_from': payment_data.get('paid_from'),
+                    'paid_from_account_currency': payment_data.get('paid_from_account_currency'),
+                    'paid_to': payment_data.get('paid_to'),
+                    'paid_to_account_currency': payment_data.get('paid_to_account_currency'),
+                    'paid_amount': payment_data.get('paid_amount'),
+                    'source_exchange_rate': payment_data.get('source_exchange_rate'),
+                    'received_amount': payment_data.get('received_amount'),
+                    'target_exchange_rate': payment_data.get('target_exchange_rate'),
+                    'custom_codigo': payment_data.get('custom_codigo'),
+                    'party_name': payment_data.get('party_name'),
+                    'mode_of_payment': payment_data.get('mode_of_payment'),
+                    'total_allocated_amount': payment_data.get('total_allocated_amount'),
+                    'factura': invoice_reference_name,
+                    'total_amount': payment_data.get('paid_amount')  # Considerando que o total_amount é o paid_amount
+                }
+                detailed_payments.append(formatted_payment)
+
+            except requests.exceptions.RequestException as payment_error:
+                detailed_payments.append({'id': payment_id, 'error': str(payment_error)})
+
+        # Pega os últimos 20 registros, se necessário
+        last_20_payments = detailed_payments[-20:]
+
+        return jsonify(last_20_payments)
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+
 def fetch_facturas():
     url = f'{base_url}/api/resource/Sales Invoice'
     params = {
@@ -73,6 +157,7 @@ def fetch_item_details(item_name):
         return {}
 
 
+# Esta funcao,e responsavel por gerar o excel das facturas de vendas(sales Invoices)
 def generate_excel(data):
     df = pd.DataFrame(data)
     output = BytesIO()
@@ -82,6 +167,7 @@ def generate_excel(data):
     return output
 
 
+# Este metodo gera de forma automatica, facturas de 6 em 6 horas.
 def fetch_and_process_invoices():
     print("Iniciando o processo de busca e processamento das faturas...")
     invoices = fetch_facturas()
@@ -211,18 +297,17 @@ def fetch_and_process_invoices():
     print(f"Arquivo salvo como {file_path} na raiz do projeto")
 
 
-# Configuração do agendador
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=fetch_and_process_invoices, trigger="interval", minutes=3600)
-scheduler.start()
 
 
+
+# Esta funcao busca a lista de factura de Vendas Pendentes.
 @app.route('/facturas', methods=['GET'])
 def get_invoices():
     invoices = fetch_facturas()
     return jsonify(invoices)
 
 
+# Esta funcao serve para gerar o excel num ambiente de teste como, postman, insomia etc.
 @app.route('/facturas/excel', methods=['GET'])
 def get_invoices_excel():
     invoices = fetch_facturas()
@@ -352,10 +437,194 @@ def get_invoices_excel():
     return jsonify({"message": f"Arquivo salvo como {file_path} na raiz do projeto"})
 
 
-@app.route('/item/update', methods=['GET'])
-def update_item():
-    # Código para atualizar itens (como no seu código original)
-    return jsonify({"message": "Items updated"})
+# NOTA: O CODIGO ABAIXO E REFERENTE A EXPORTACAO DE RECIBOS DE PAGAMENTOS APENAS
+# Esta funcao e usada pelo metodo get_all_payment para gerar o ficheiro excel com os recibos
+def generate_payment_excel(data):
+    # Criação do DataFrame
+    df = pd.DataFrame(data)
+    output = BytesIO()
+    df.to_excel(output, index=False, engine='openpyxl')
+    output.seek(0)  # Voltar ao início do BytesIO
+    return output
+
+
+# Esta e a funcao usada para gerar os recibos, usando postman, insomia ou algum ambiente de testes
+@app.route('/payments/excel', methods=['GET'])
+def get_all_payments():
+    endpoint = "/api/resource/Payment Entry"
+    url = base_url + endpoint
+
+    try:
+        # Primeiro, obtenha a lista de IDs
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+
+        # Extraia os IDs dos pagamentos
+        payment_ids = [entry.get('name') for entry in data.get('data', [])]
+
+        # Obtenha detalhes para cada ID
+        detailed_payments = []
+        for payment_id in payment_ids:
+            detail_url = f"{base_url}/api/resource/Payment Entry/{payment_id}"
+            try:
+                detail_response = requests.get(detail_url, headers=headers)
+                detail_response.raise_for_status()
+                payment_details = detail_response.json()
+
+                # Adiciona o ID aos detalhes do pagamento
+                payment_data = payment_details.get('data', {})
+                payment_data['id'] = payment_id
+
+                # Adiciona os dados de referência da fatura
+                invoice_reference_name = None
+                payment_references = payment_data.get('references', [])
+                for reference in payment_references:
+                    if reference.get('reference_doctype') == 'Sales Invoice':
+                        invoice_reference_name = reference.get('reference_name')
+                        break  # Presumindo que há apenas uma fatura por pagamento
+
+                # Buscar dados do cliente e o campo 'custom_codigo'
+                customer_id = payment_data.get('party')  # Ajuste o nome do campo se necessário
+                if customer_id:
+                    customer_url = f"{base_url}/api/resource/Customer/{customer_id}"
+                    try:
+                        customer_response = requests.get(customer_url, headers=headers)
+                        customer_response.raise_for_status()
+                        customer_data = customer_response.json()
+                        custom_codigo = customer_data.get('data', {}).get('custom_codigo', None)
+                        payment_data['custom_codigo'] = custom_codigo
+                    except requests.exceptions.RequestException as customer_error:
+                        payment_data['custom_codigo_error'] = str(customer_error)
+
+                # Formata o resultado conforme solicitado
+                formatted_payment = {
+                    'id': payment_data.get('id'),
+                    'naming_series': payment_data.get('naming_series'),
+                    'payment_type': payment_data.get('payment_type'),
+                    'posting_date': payment_data.get('posting_date'),
+                    'company': payment_data.get('company'),
+                    'paid_from': payment_data.get('paid_from'),
+                    'paid_from_account_currency': payment_data.get('paid_from_account_currency'),
+                    'paid_to': payment_data.get('paid_to'),
+                    'paid_to_account_currency': payment_data.get('paid_to_account_currency'),
+                    'paid_amount': payment_data.get('paid_amount'),
+                    'source_exchange_rate': payment_data.get('source_exchange_rate'),
+                    'received_amount': payment_data.get('received_amount'),
+                    'target_exchange_rate': payment_data.get('target_exchange_rate'),
+                    'custom_codigo': payment_data.get('custom_codigo'),
+                    'party_name': payment_data.get('party_name'),
+                    'mode_of_payment': payment_data.get('mode_of_payment'),
+                    'total_allocated_amount': payment_data.get('total_allocated_amount'),
+                    'factura': invoice_reference_name,
+                    'total_amount': payment_data.get('paid_amount')  # Considerando que o total_amount é o paid_amount
+                }
+                detailed_payments.append(formatted_payment)
+
+            except requests.exceptions.RequestException as payment_error:
+                detailed_payments.append({'id': payment_id, 'error': str(payment_error)})
+
+        # Pega os últimos 20 registros, se necessário
+        last_20_payments = detailed_payments[-50:]
+
+        # Gera o arquivo Excel
+        output = generate_payment_excel(last_20_payments)
+        file_path = 'payments.xlsx'
+        with open(file_path, 'wb') as f:
+            f.write(output.getvalue())
+
+        return jsonify({"message": f"Arquivo salvo como {file_path} na raiz do projeto"})
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+
+def fetch_and_save_payments():
+    endpoint = "/api/resource/Payment Entry"
+    url = base_url + endpoint
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        payment_ids = [entry.get('name') for entry in data.get('data', [])]
+
+        detailed_payments = []
+        for payment_id in payment_ids:
+            detail_url = f"{base_url}/api/resource/Payment Entry/{payment_id}"
+            try:
+                detail_response = requests.get(detail_url, headers=headers)
+                detail_response.raise_for_status()
+                payment_details = detail_response.json()
+
+                payment_data = payment_details.get('data', {})
+                payment_data['id'] = payment_id
+
+                invoice_reference_name = None
+                payment_references = payment_data.get('references', [])
+                for reference in payment_references:
+                    if reference.get('reference_doctype') == 'Sales Invoice':
+                        invoice_reference_name = reference.get('reference_name')
+                        break
+
+                customer_id = payment_data.get('party')
+                if customer_id:
+                    customer_url = f"{base_url}/api/resource/Customer/{customer_id}"
+                    try:
+                        customer_response = requests.get(customer_url, headers=headers)
+                        customer_response.raise_for_status()
+                        customer_data = customer_response.json()
+                        custom_codigo = customer_data.get('data', {}).get('custom_codigo', None)
+                        payment_data['custom_codigo'] = custom_codigo
+                    except requests.exceptions.RequestException as customer_error:
+                        payment_data['custom_codigo_error'] = str(customer_error)
+
+                formatted_payment = {
+                    'id': payment_data.get('id'),
+                    'naming_series': payment_data.get('naming_series'),
+                    'payment_type': payment_data.get('payment_type'),
+                    'posting_date': payment_data.get('posting_date'),
+                    'company': payment_data.get('company'),
+                    'paid_from': payment_data.get('paid_from'),
+                    'paid_from_account_currency': payment_data.get('paid_from_account_currency'),
+                    'paid_to': payment_data.get('paid_to'),
+                    'paid_to_account_currency': payment_data.get('paid_to_account_currency'),
+                    'paid_amount': payment_data.get('paid_amount'),
+                    'source_exchange_rate': payment_data.get('source_exchange_rate'),
+                    'received_amount': payment_data.get('received_amount'),
+                    'target_exchange_rate': payment_data.get('target_exchange_rate'),
+                    'custom_codigo': payment_data.get('custom_codigo'),
+                    'party_name': payment_data.get('party_name'),
+                    'mode_of_payment': payment_data.get('mode_of_payment'),
+                    'total_allocated_amount': payment_data.get('total_allocated_amount'),
+                    'factura': invoice_reference_name,
+                    'total_amount': payment_data.get('paid_amount')
+                }
+                detailed_payments.append(formatted_payment)
+
+            except requests.exceptions.RequestException as payment_error:
+                detailed_payments.append({'id': payment_id, 'error': str(payment_error)})
+
+        last_20_payments = detailed_payments[-50:]
+
+        output = generate_payment_excel(last_20_payments)
+        file_path = 'payments.xlsx'
+        with open(file_path, 'wb') as f:
+            f.write(output.getvalue())
+
+        print(f"Arquivo salvo como {file_path} na raiz do projeto")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro: {str(e)}")
+
+
+
+# Configuração do CronJob
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=fetch_and_process_invoices, trigger="interval", minutes=1) #Gera a factura
+scheduler.add_job(func=fetch_and_save_payments, trigger="interval", minutes=1) #Gera os Recibos
+scheduler.start()
+
 
 
 if __name__ == '__main__':
